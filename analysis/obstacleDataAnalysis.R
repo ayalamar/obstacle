1f380974baca4d4d6ec2fd58d29f57b4b0564484
Maria Ayala <mayala@research.baycrest.org>
2019-02-13 11:00:20 -0500

    edit analysis script

--- analysis/obstacleDataAnalysis.R
+++ analysis/obstacleDataAnalysis.R
@@ -5,18 +5,14 @@ library(dplyr)
 ## dual group 
 
 obsdual <- read.csv('DualObs_Data0305_DUALONLY.csv', header = TRUE)
-
 obsdual <- tbl_df(obsdual)
+
+# training data
 obs_dual_train <- obsdual %>% filter(condition == 'train_dual') # looking only at training
-
-# get means
-
 taskmeans<- obs_dual_train %>% group_by(subject) %>% group_by(trial) %>% 
   summarise(Mean_RMSExy = mean(RMSExy, na.rm=TRUE), SD_RMSExy = sd(RMSExy, na.rm=TRUE), 
             SEM_RMSExy = SD_RMSExy/sqrt(length(unique(subject))))
-
-# plot
-
+  # plot
 taskplot <- ggplot(data=taskmeans, aes(x=trial, y=Mean_RMSExy)) +
   geom_line() + 
   geom_ribbon(aes(ymin=Mean_RMSExy-SEM_RMSExy, ymax=Mean_RMSExy+SEM_RMSExy),
@@ -28,8 +24,8 @@ taskplot <- ggplot(data=taskmeans, aes(x=trial, y=Mean_RMSExy)) +
 
 print(taskplot)
 
-# some basic statistics 
-# adaptation (dv -- RMSE)
+  # some basic statistics 
+  # adaptation (dv -- RMSE)
 
 block1<- obs_dual_train %>% filter(trial %in% c(0)) %>% group_by(subject) %>% summarise(RMSE = mean(RMSExy, na.rm=TRUE), block = mean(block5))
 blocklast<- obs_dual_train %>% filter(trial %in% c(355,356,357,358,359)) %>% group_by(subject) %>% summarise(RMSE = mean(RMSExy, na.rm=TRUE), block = ceiling(mean(block5)))
@@ -42,9 +38,36 @@ RM_RMSE <- aov(RMSE ~ block + Error(subject/block), data=adaptdf)
 summary(RM_RMSE)
 # the effect of block is significant (F(1,22)=8.352, p=0.0085)
 
-# analyze per target location
+# NOTE --- analyze per target location
 
-############## single controls
+# nocursor data
+obs_dual_AE <- obsdual %>% filter(condition == c("nocur_1", "nocur_2")) # only nocursors 
+# AEmeans<- obs_dual_AE %>% group_by(subject) %>% group_by(trial) %>% 
+#   summarise(Mean_angmaxvel = mean(angmaxvel, na.rm=TRUE), SD_angmaxvel = sd(angmaxvel, na.rm=TRUE), 
+#             SEM_angmaxvel = SD_angmaxvel/sqrt(length(unique(subject))))
+
+for (rot in sort(unique(obs_dual_AE$rotationval))){
+
+nocur1<- obs_dual_AE %>% filter(rotationval == rot) %>% filter(condition == "nocur_1") %>% filter(trial %in% c(25,26,27,28,29)) %>%
+  group_by(subject) %>% summarise(pv = mean(angmaxvel, na.rm=TRUE), block = ceiling(mean(block5))) %>% mutate(task = 'nocur1')
+# boxplot(nocur1$pv)
+nocur2<- obs_dual_AE %>% filter(rotationval == rot) %>% filter(condition == "nocur_2") %>% filter(trial %in% c(0, 1, 2, 3, 4)) %>% 
+  group_by(subject) %>% summarise(pv = mean(angmaxvel, na.rm=TRUE), block = ceiling(mean(block5))) %>% mutate(task = 'nocur2')
+# boxplot(nocur2$pv)
+nocurdf<- rbind(nocur1,nocur2)
+
+nocurdf$task <- factor(nocurdf$task)
+nocurdf$subject <- factor(nocurdf$subject)
+
+AE_dual_n <- nocur2$pv - nocur1$pv
+t.test(AE_dual_n, mu = 0)
+# dual CCW - t(17) = -2.29, p=0.035
+# dual CW t(20) = 2.11, p = 0.047
+
+}
+
+####################################################################
+########################## single controls #########################
 # load
 
 obssingle <- read.csv('DualObs_Data0305_SINGLESONLY.csv', header = TRUE)
@@ -96,6 +119,38 @@ obssingle <- obssingle %>% filter(condition == c('train_CW', 'train_CCW'))
     
     ## CCW - (F(1,9) = 20.81, p=0.00136)
     ## CW - (F(1,8) = 17.57, p=0.00303)
+    
   }
 
-#### NOTE -- just sort these out by subject .. check spread sheet
+# nocursor data
+obs_single_AE <- obssingle %>% filter(condition == c("nocur_1", "nocur_2")) # only nocursors 
+# AEmeans<- obs_single_AE %>% group_by(subject) %>% group_by(trial) %>% 
+#   summarise(Mean_angmaxvel = mean(angmaxvel, na.rm=TRUE), SD_angmaxvel = sd(angmaxvel, na.rm=TRUE), 
+#             SEM_angmaxvel = SD_angmaxvel/sqrt(length(unique(subject))))
+
+for (rot in sort(unique(obs_single_AE$rotationval))){
+ 
+  nocur1<- obs_single_AE %>% filter(rotgroup == rot) %>% filter(condition == "nocur_1") %>% filter(trial %in% c(25,26,27,28,29)) %>%
+    group_by(subject) %>% summarise(pv = mean(angmaxvel, na.rm=TRUE), block = ceiling(mean(block5))) %>% mutate(task = 'nocur1')
+  # boxplot(nocur1$pv)
+  # nocur1 <- nocur1[-c(2,3),] # outliers
+  nocur2<- obs_single_AE %>% filter(rotgroup == rot) %>% filter(condition == "nocur_2") %>% filter(trial %in% c(0, 1, 2, 3, 4)) %>% 
+    group_by(subject) %>% summarise(pv = mean(angmaxvel, na.rm=TRUE), block = ceiling(mean(block5))) %>% mutate(task = 'nocur2')
+  # boxplot(nocur2$pv)
+  # nocur2 <- nocur2[-c(15),] # outlying participant from training
+  nocurdf<- rbind(nocur1,nocur2)
+  
+  nocurdf$task <- factor(nocurdf$task)
+  nocurdf$subject <- factor(nocurdf$subject)
+  
+  ggplot(nocurdf, aes(task,pv)) +
+    + geom_point()
+  
+  AE_single_n <- nocur2$pv - nocur1$pv
+  t.test(AE_single_n, mu = 0)
+  
+  
+  # single CW t = 1.9947, df = 12, p-value = 0.0693
+  # single CCW t = -1.564, df = 10, p-value = 0.1489
+
+}
